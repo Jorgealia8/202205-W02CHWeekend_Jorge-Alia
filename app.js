ca3339dd5847f06db757d8694765306e1598b8b5
Jorge Alia <jorgealia94@gmail.com>
2022-05-20 19:59:14 +0200

    Update app new array

---
+++ app.js
@@ -0,0 +1,14 @@
+// let rows = 5;
+// let columns = 5;
+
+// let array = new Array(rows);
+// let secondArray = new Array(columns);
+
+let array = [
+[0,0,0,0,0],
+[0,0,0,0,0],
+[0,0,0,0,0],
+[0,0,0,0,0],
+[0,0,0,0,0],
+]
+for (let i= 0; i < )
